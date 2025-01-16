114e910a1b086e3f0a223f8ef8dda8d4957ca7fe
qiansui <qiansui159@qq.com>
2025-01-16 17:06:01 +0800

    the code

---
+++ cs.cpp
@@ -0,0 +1,30 @@
+//Author: Qiansui
+//Time: 2024-12-27 11:33
+#include<bits/stdc++.h>
+#define ll long long
+#define debug(x) cout << #x << " = " << x << '\n';
+#define debug2(x,y) cout << #x << " = " << x << " " << #y << " = "<< y << '\n';
+
+// using namespace std;
+
+void solve(){
+    std::cout << "yes\n";
+    return ;
+}
+
+signed main(){
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
+    int _ = 1;
+    // std::cin >> _;
+    while(_ --){
+        solve();
+    }
+    return 0;
+}
+//    ___    _                                 _ 
+//   / _ \  (_)   __ _   _ __    ___   _   _  (_)
+//  | | | | | |  / _` | | '_ \  / __| | | | | | |
+//  | |_| | | | | (_| | | | | | \__ \ | |_| | | |
+//   \__\_\ |_|  \__,_| |_| |_| |___/  \__,_| |_|
